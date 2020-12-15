380cebb4f8caeea85aac21f8b216362b04dd2932
tarrows <c10t@fa.ke>
2020-12-15 23:49:44 +0900

    WIP appbar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import styles from './Appbar.module.css'
 
 export type Props = {}
 
 export const Appbar: React.FC<Props> = () => {
   return (
     <nav>
-      <div>
+      <div className={styles.header}>
         <div>
           <div></div>
         </div>
